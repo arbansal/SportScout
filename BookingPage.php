57bcd26d25835088739c188332f0b55e3ef04ddc
Arpit Bansal <arpitbans8@gmail.com>
2019-12-18 17:12:30 -0500

    booking

--- BookingPage.php
+++ BookingPage.php
@@ -1,3 +1,51 @@
+<head>
+    <style type="text/css">
+    body{
+    background-image: url('images/background.jpg');
+    background-repeat: repeat;
+    
+}
+
+table{
+    margin-top: 10px;
+    font-family: sans-serif; 
+    color: #3da8c6;
+    width:500px;
+    border:2px;
+    background-image: url('images/tableBack.jpg');   
+}
+
+
+
+#container {
+ /*stitching*/
+ color: white;
+ outline: 1px dashed #98abb9;
+ outline-offset: -5px;
+ padding-top:10px;  
+ background-color: Coral;
+ height: 50px;
+ width: 459px;
+ 
+ /*shadow*/
+ -webkit-box-shadow: 2px 2px 2px #000;
+ -moz-box-shadow: 2px 2px 2px #000;
+ box-shadow: 2px 2px 2px #000;
+}
+
+</style>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,200,100,300' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Raleway:400,200,100,300' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="./home2.css">
+    </head>
+
+
 <script type="text/javascript">
 var today = new Date();
 var dd = today.getDate();
@@ -14,36 +62,61 @@ today = yyyy+'-'+mm+'-'+dd;
 document.getElementById("datefield").setAttribute("max", today);
 </script>
 
-
+<center>
 <form action='creditCard.php' method='post'>
+<table cellspacing="5px" cellpadding="10px">
 
-Date: 	<input id="datefield" type='date' min='2020-01-01' name="bookDate"></input>
-<br> 
-Time:   <select name="timeValue">
-		<option value="1000am-1100am">10:00AM-11:00AM</option>
-		<option value="1100AM-1200PM">11:00AM-12:00PM</option>
-		<option value="1200PM-100PM">12:00PM-1:00PM</option>
-		<option value="100PM-200PM">1:00PM-2:00PM</option>
-		<option value="200PM-300PM">2:00PM-3:00PM</option>
-		<option value="300PM-400PM">3:00PM-4:00PM</option>
-		<option value="400PM-500PM">4:00PM-5:00PM</option>
+<tr>
+<td>
+Date: </td>
+<td>
+<input id="datefield" type='date' min='2020-01-01' name="bookDate"></input>
+</td>
+
+<td>
+Time:
+</td>
+
+<td>
+        <select name="timeValue">
+		<option value="10AM-11AM">10:00AM-11:00AM</option>
+		<option value="11AM-12PM">11:00AM-12:00PM</option>
+		<option value="12PM-1PM">12:00PM-1:00PM</option>
+		<option value="1PM-2PM">1:00PM-2:00PM</option>
+		<option value="2PM-3PM">2:00PM-3:00PM</option>
+		<option value="3PM-4PM">3:00PM-4:00PM</option>
+		<option value="4PM-5PM">4:00PM-5:00PM</option>
 		</select>
-<br>
-Credit Card Details:<input type="text" ><br>
-Exp Month:<input type="text" placeholder="10/21">
-<br>
+</td>
+</tr>
+        <td>
+        Credit Card Details:
+        </td>
+<td>
+<input type="text" ></td>
+<td>
+Exp Month:
+</td>
+<td>
+    <input type="text" placeholder="10/21">
+</td>
 
-<input type="submit" value="BOOK" name="BookedPlace"/>
-
+<tr>
+<td colspan="4">
+<CENTER><br>
+<button type="submit" id="button" class="btnAll" name="BookedPlace">BOOK</button>
+</CENTER>
+</td></tr>
+</table>
 </form>
-
+</center>
 <?php
 // echo $_POST['placeId'];
 // echo $_POST['playersUsername'];
 // echo $_POST['playersEmail'];
 // echo $_POST['managersEmail'];
 
-echo "<h2>Already filled values</h2>";
+echo "<h2 style='background-color:lightsalmon;color:white;'>Already filled values</h2>";
 echo "<br>";
 
 if(isset($_POST['bookRedirection'])){
@@ -67,9 +140,10 @@ $sql="SELECT *  FROM bookings WHERE venueName='$placeId'";
     
     if (mysqli_num_rows($res) > 0) { 
     
-    echo "<table>";
+    echo "<center>";
+    echo "<table cellspacing='20px' cellpadding='20px'>";
+    echo "<tr style='background-color:#138496;color:white;'><td>Date of Booking</td><td>Time (in hours)</td></tr>";
        while ($row = mysqli_fetch_array($res)) { 
-            
             
             echo "<tr>"; 
             
@@ -79,7 +153,7 @@ $sql="SELECT *  FROM bookings WHERE venueName='$placeId'";
 
         }
         echo "</table>";
-
+    echo "</center>";
 }
 
 }
--- creditCard.php
+++ creditCard.php
@@ -18,15 +18,19 @@ $db = mysqli_connect("db.soic.indiana.edu", "p565f18_arbansal", "sqlmypass123", 
 $sql2="insert into bookings values('$placeId','$playersUsername','$date','$timeValue')";
  
 if ($res = mysqli_query($db, $sql2)) {
+        $message="Successfully Booked for ".$date." time ".$timeValue." Venue ".$placeId;
+        mail($playersEmail,"Venue Booked" , $message);
+        mail($managersEmail,"Venue Booked",$message);
 echo "<script>
         alert('Successfully Booked');
+        
         window.close();
         </script>";
 }
 else{
 	"<script>
         alert('Successfully Booked');
-        
+        window.location.href='BookingPage.php';
         </script>";
 }
 
